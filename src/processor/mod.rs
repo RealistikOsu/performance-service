e3daaf697c8da7c2c018006281bf16dad6f36458
tsunyoku <mbruhyo@gmail.com>
2022-12-17 16:30:00 +0100

    chore: update rework rev

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -113,7 +113,7 @@ async fn handle_queue_request(
         _ => unreachable!(),
     };
 
-    let mut scores: Vec<RippleScore> = sqlx::query_as(
+    let scores: Vec<RippleScore> = sqlx::query_as(
         &format!(
             "SELECT s.id, s.beatmap_md5, s.userid, s.score, s.max_combo, s.full_combo, s.mods, s.300_count, 
             s.100_count, s.50_count, s.katus_count, s.gekis_count, s.misses_count, s.time, s.play_mode, s.completed, 
